d9cd6dc57e6f5859ab62597a415e9672773a6de6
melvinchia3636 <melvinchia623600@gmail.com>
2024-10-19 07:55:45 +0800

    chore: changed from page router to app router

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="bg-zinc-950 font-sans w-full min-h-dvh text-zinc-100 flex items-center justify-center text-2xl tracking-[0.5rem] uppercase">
-      Lifeforge.
-    </main>
-  );
-}
