8101df5b79e82bfbf1aaf637b5533e54edf2b8e9
melvinchia3636 <melvinchia623600@gmail.com>
2024-10-19 11:10:05 +0800

    feat: added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="w-full h-dvh flex">{children}</body>
+    <html lang="en" className="h-dvh w-full overflow-hidden">
+      <body className="w-full h-dvh overflow-y-auto bg-white dark:bg-zinc-900 text-zinc-700 dark:text-zinc-100">
+        <main className="flex h-full">{children}</main>
+        <footer className="absolute bottom-4 left-4 text-zinc-300 dark:text-zinc-700 text-sm">
+          <p>in-dev v.24w42</p>
+        </footer>
+      </body>
     </html>
   );
 }
